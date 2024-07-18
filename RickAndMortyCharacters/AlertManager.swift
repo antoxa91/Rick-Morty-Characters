5129d970e0ead9c129feea897fa6cc78dc18c132
Anton Stafeev <stafeevt@mail.ru>
2024-07-19 03:12:10 +0500

    add AlertManger and error handling

---
+++ RickAndMortyCharacters/AlertManager.swift
@@ -0,0 +1,8 @@
+//
+//  AlertManager.swift
+//  RickAndMortyCharacters
+//
+//  Created by Антон Стафеев on 19.07.2024.
+//
+
+import Foundation
---
+++ RickAndMortyCharacters/Networking/NetworkError.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkError.swift
+//  RickAndMortyCharacters
+//
+//  Created by Антон Стафеев on 19.07.2024.
+//
+
+import Foundation
