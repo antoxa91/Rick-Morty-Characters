867819e6e8b9a972589e7bcbdc65a833b0a4990d
Anton Stafeev <stafeevt@mail.ru>
2024-07-20 00:56:22 +0500

    update

---
+++ RickAndMortyCharacters/Views/FooterLoaderView.swift
@@ -0,0 +1,43 @@
+//
+//  FooterLoaderTableView.swift
+//  RickAndMortyCharacters
+//
+//  Created by Антон Стафеев on 20.07.2024.
+//
+
+import UIKit
+
+final class FooterLoaderTableView: UIView {
+    static let identifier = "RMFooterLoadingCollectionReusableView"
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        addSubview(spinner)
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    public func startAnimating() {
+        spinner.startAnimating()
+    }
+}
