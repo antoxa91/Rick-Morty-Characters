38f526df1aaa6fdfafa8ffa1e66aa7c135f08b86
Anton Stafeev <stafeevt@mail.ru>
2024-07-20 16:22:43 +0500

    make code cleaner

--- RickAndMortyCharacters/Networking/CharactersLoaderService.swift
+++ RickAndMortyCharacters/Networking/CharactersLoaderService.swift
@@ -8,25 +8,32 @@
 import Foundation
 import OSLog
 
-protocol CharactersLoaderProtocol {
-    func fetchInitialCharacters(completion: @escaping (Result<AllCharactersResponse, NetworkError>) -> Void)
-    func fetchAdditionalCharacters(url: URL, completion: @escaping (Result<AllCharactersResponse, NetworkError>) -> Void)
+protocol CharactersLoadable: AnyObject {
+    typealias DownloadHandler = (Result<AllCharactersResponse, NetworkError>) -> Void
+
+    func fetchInitialCharacters(completion: @escaping DownloadHandler)
+    func fetchAdditionalCharacters(url: URL, completion: @escaping DownloadHandler)
+    func fetchSearchableCharacters(name: String, completion: @escaping DownloadHandler)
 }
 
-final class CharactersLoaderService: CharactersLoaderProtocol {
+final class CharactersLoaderService {
     private let networkService: NetworkServiceProtocol
-    
+
     init(networkService: NetworkServiceProtocol) {
         self.networkService = networkService
     }
     
-    func fetchInitialCharacters(completion: @escaping (Result<AllCharactersResponse, NetworkError>) -> Void) {
+    private func buildURL(queryItems: [URLQueryItem]?) -> URL? {
         var urlComponents = URLComponents()
         urlComponents.scheme = "https"
         urlComponents.host = "rickandmortyapi.com"
         urlComponents.path = "/api/character"
-        
-        guard let url = urlComponents.url else {
+        urlComponents.queryItems = queryItems
+        return urlComponents.url
+    }
+    
+    private func fetchData(from url: URL?, completion: @escaping DownloadHandler) {
+        guard let url = url else {
             completion(.failure(NetworkError.invalidURL))
             return
         }
@@ -37,18 +44,25 @@ final class CharactersLoaderService: CharactersLoaderProtocol {
                 completion(.success(response))
             case .failure(let error):
                 Logger.network.error("Ошибка: \(error.localizedDescription)")
+                completion(.failure(error))
             }
         }
     }
+}
+
+// MARK: - CharactersLoadable
+extension CharactersLoaderService: CharactersLoadable {
+    func fetchInitialCharacters(completion: @escaping DownloadHandler) {
+        let url = buildURL(queryItems: nil)
+        fetchData(from: url, completion: completion)
+    }
     
-    func fetchAdditionalCharacters(url: URL, completion: @escaping (Result<AllCharactersResponse, NetworkError>) -> Void) {
-        networkService.fetchData(awaiting: AllCharactersResponse.self, url: url) { result in
-            switch result {
-            case .success(let response):
-                completion(.success(response))
-            case .failure(let error):
-                Logger.network.error("Ошибка: \(error.localizedDescription)")
-            }
-        }
+    func fetchAdditionalCharacters(url: URL, completion: @escaping DownloadHandler) {
+        fetchData(from: url, completion: completion)
+    }
+    
+    func fetchSearchableCharacters(name: String, completion: @escaping DownloadHandler) {
+        let url = buildURL(queryItems: [URLQueryItem(name: "name", value: name)])
+        fetchData(from: url, completion: completion)
     }
 }
