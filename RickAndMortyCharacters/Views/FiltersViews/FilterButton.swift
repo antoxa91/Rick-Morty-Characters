2e860e89944854bd4b4c34327631b10b9744140a
Anton Stafeev <stafeevt@mail.ru>
2024-07-29 02:28:00 +0500

    animate selected filter btn

--- RickAndMortyCharacters/Views/FiltersViews/FilterButton.swift
+++ RickAndMortyCharacters/Views/FiltersViews/FilterButton.swift
@@ -21,7 +21,19 @@ final class FilterButton: UIButton {
     override var isSelected: Bool {
         didSet {
             updateAppearance()
+            animateButtonTap()
         }
+    }
+    
+    private func setup(title: String) {
+        heightAnchor.constraint(equalToConstant: 36).isActive = true
+        setTitleColor(AppColorEnum.text.color, for: .normal)
+        setTitle(title, for: .normal)
+        titleLabel?.font = .IBMPlexSans()
+        layer.cornerRadius = 14
+        layer.borderColor = AppColorEnum.cellBackground.color.cgColor
+        layer.borderWidth = 3
+        contentEdgeInsets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
     }
     
     private func updateAppearance() {
@@ -36,14 +48,22 @@ final class FilterButton: UIButton {
         }
     }
     
-    private func setup(title: String) {
-        heightAnchor.constraint(equalToConstant: 36).isActive = true
-        setTitleColor(AppColorEnum.text.color, for: .normal)
-        setTitle(title, for: .normal)
-        titleLabel?.font = .IBMPlexSans()
-        layer.cornerRadius = 14
-        layer.borderColor = AppColorEnum.cellBackground.color.cgColor
-        layer.borderWidth = 3
-        contentEdgeInsets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+    private func animateButtonTap() {
+        UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseInOut) {
+            self.transform = .init(scaleX: 0.95, y: 0.95)
+        } completion: { _ in
+            self.transform = .identity
+        }
+
+        let pulse = CASpringAnimation(keyPath: "transform.scale")
+        pulse.duration = 0.2
+        pulse.fromValue = 1.0
+        pulse.toValue = 1.1
+        pulse.autoreverses = true
+        pulse.repeatCount = 1
+        pulse.initialVelocity = 0.5
+        pulse.damping = 1.0
+        
+        layer.add(pulse, forKey: "pulse")
     }
 }
